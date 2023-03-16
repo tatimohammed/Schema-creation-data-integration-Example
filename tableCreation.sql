2a385ca74b9482be2f099c77cefaf7fd38325592
TATI Mohammed <95311883+tati2002med@users.noreply.github.com>
2023-03-16 20:27:31 +0100

    Update tableCreation.sql

--- tableCreation.sql
+++ tableCreation.sql
@@ -1,3 +1,9 @@
+----------------------------------------------------------------------------------------------------------------
+-- Note:                                                                                                      --
+--      - When you are creating a database always start with the tables with no constraints (Foreign keys).   --
+----------------------------------------------------------------------------------------------------------------
+
+-- Creating the user table
 CREATE TABLE User (
     user_id INT PRIMARY KEY,
     username VARCHAR(100),
@@ -5,12 +11,14 @@ CREATE TABLE User (
     password VARCHAR(100)
 );
 
+-- Creating the product table
 CREATE TABLE Product (
     product_id INT PRIMARY KEY,
     product_name VARCHAR(100),
     price DECIMAL(10,2)
 );
 
+-- Creating the order table
 CREATE TABLE Order (
     order_id INT PRIMARY KEY,
     user_id INT,
