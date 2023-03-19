4cb32933cea6224c6ce3e5d6c8ac2c08e6cd6427
Said Ait Ouakour <113073287+moon41x4@users.noreply.github.com>
2023-03-19 08:15:11 +0000

    Update sql_script1.sql

--- sql_script1.sql
+++ sql_script1.sql
@@ -1,4 +1,4 @@
 ---------- creating table  table1 ------------------
-CREATE IF NOT EXIST TABLE table1 { ATTR1 INT,
-ATTR2 VARCHAR }
+CREATE IF NOT EXIST TABLE table1 ( ATTR1 INT,
+ATTR2 VARCHAR )
 
