c13c02f85c2f763b5bc6571e0ec16fc83ae8dfb1
Said Ait Ouakour <113073287+moon41x4@users.noreply.github.com>
2023-03-19 08:14:51 +0000

    Update script2.sql

--- script2.sql
+++ script2.sql
@@ -1,12 +1,12 @@
 ---------- creating table  table1 ------------------
-CREATE IF NOT EXIST TABLE table1 { ATTR1 INT,
-ATTR2 VARCHAR }
+CREATE IF NOT EXIST TABLE table1 ( ATTR1 INT,
+ATTR2 VARCHAR )
 
 ---------- creating table  table2 ------------------
-CREATE IF NOT EXIST TABLE table2 { ATTR1 INT,
-ATTR2 VARCHAR }
+CREATE IF NOT EXIST TABLE table2 ( ATTR1 INT,
+ATTR2 VARCHAR )
 
 ---------- creating table  table3 ------------------
-CREATE IF NOT EXIST TABLE table3 { ATTR1 INT,
-ATTR2 VARCHAR }
+CREATE IF NOT EXIST TABLE table3 ( ATTR1 INT,
+ATTR2 VARCHAR )
 
